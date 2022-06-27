95daf0ab56d9b4e8f0507011528f75c5023c1bc2
stts <it.spectre.ru@gmail.com>
2022-06-27 12:26:55 +0500

    With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.

--- 100/main.py
+++ 100/main.py
@@ -32,13 +32,37 @@ for i in range(2000, 3201):
 # 40320
  
 
-def fact(x):
-    if x == 0:
-        return 1
-    return x * fact(x - 1)
+# def fact(x):
+#     if x == 0:
+#         return 1
+#     return x * fact(x - 1)
  
-x=int(input())
-print(fact(x))
+# x=int(input())
+# print(fact(x))
+
+
+
+
+
+### Question 3
+
+# With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.
+# Suppose the following input is supplied to the program:
+# 8
+# Then, the output should be:
+# {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+ 
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+# Consider use dict()
+ 
+
+n=int(input())
+d=dict()
+for i in range(1,n+1):
+    d[i]=i*i
+ 
+print(d)
 
 
 
